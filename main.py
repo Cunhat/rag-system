86e2f88191928bc395ea461a6042da4da86e79ca
Tiago Cunha <tiago92cunha@gmail.com>
2026-04-14 23:10:47 +0100

    Add fastapi and inngest init

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from rag-system!")
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
 
 
-if __name__ == "__main__":
-    main()
+
+# Create an Inngest client
+inngest_client = inngest.Inngest(
+    app_id="rag-system",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
+
+# Create an Inngest function
+@inngest_client.create_function(
+    fn_id="my_function",
+    # Event that triggers this function
+    trigger=inngest.TriggerEvent(event="app/my_function"),
+)
+async def my_function(ctx: inngest.Context) -> str:
+    ctx.logger.info(ctx.event)
+    return "done"
+
+app = FastAPI()
+
+inngest.fast_api.serve(app, inngest_client, [])

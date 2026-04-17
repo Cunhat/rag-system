f14e08495826f1e12ff517125a639f7f880a9464
Tiago Cunha <tiago92cunha@gmail.com>
2026-04-17 20:41:17 +0100

    Add vector db

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from fastapi import FastAPI
 import inngest
 import inngest.fast_api
 
-
-
 # Create an Inngest client
 inngest_client = inngest.Inngest(
-    app_id="rag-system",
+    app_id="rag_system",
     logger=logging.getLogger("uvicorn"),
     is_production=False,
     serializer=inngest.PydanticSerializer()
@@ -15,14 +13,15 @@ inngest_client = inngest.Inngest(
 
 # Create an Inngest function
 @inngest_client.create_function(
-    fn_id="my_function",
+    fn_id="rag_ingest",
     # Event that triggers this function
-    trigger=inngest.TriggerEvent(event="app/my_function"),
+    trigger=inngest.TriggerEvent(event="rag/ingest"),
 )
-async def my_function(ctx: inngest.Context) -> str:
+
+async def rag_ingest(ctx: inngest.Context) -> str:
     ctx.logger.info(ctx.event)
     return "done"
 
 app = FastAPI()
 
-inngest.fast_api.serve(app, inngest_client, [])
+inngest.fast_api.serve(app, inngest_client, [rag_ingest])
